65b599a359bc9c28e823d734de88162a1e33b70d
rudravchaudhari167 <rudravchaudhari167@gmail.com>
2026-09-13 21:54:20 +0530

    Add Home link next to Shop in navigation

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -20,6 +20,7 @@ import { useWishlistStore } from "@/lib/wishlist-store";
 import SearchOverlay from "@/components/SearchOverlay";
 
 const NAV_LINKS = [
+  { href: "/", label: "Home" },
   { href: "/shop", label: "Shop" },
 ];
 
